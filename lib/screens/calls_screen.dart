a04c3de7333d19e1170f3882867265df5b814242
Payal Bhatt <payalbhatt2006@gamil.com>
2025-05-18 15:58:41 +0530

    updated call screen

--- lib/screens/calls_screen.dart
+++ lib/screens/calls_screen.dart
@@ -1,88 +1,170 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class CallsScreen extends StatelessWidget {
-  final bool isDarkMode;
+  CallsScreen({super.key});
 
-  const CallsScreen({super.key, this.isDarkMode = false});
+  final List<Map<String, String>> recentCalls = [
+    {
+      "name": "Jass",
+      "time": "Yesterday, 5:24 PM",
+      "image": "assets/uncle.png",
+      "status": "Missed audio call",
+    },
+    {
+      "name": "Raman",
+      "time": "Yesterday, 5:23 PM",
+      "image": "assets/uncle.png",
+      "status": "Outgoing call",
+    },
+    {
+      "name": "Pooja",
+      "time": "Yesterday, 1:12 PM",
+      "image": "assets/uncle.png",
+      "status": "Incoming call",
+    },
+  ];
 
   @override
   Widget build(BuildContext context) {
-    final bgColor = isDarkMode ? Colors.black : Colors.white;
-    final textColor = isDarkMode ? Colors.white : Colors.black87;
-    final List<Map<String, dynamic>> calls = [
-      {
-        "name": "Jass",
-        "time": "Today, 10:32 AM",
-        "incoming": true,
-        "video": false,
-      },
-      {
-        "name": "Raman",
-        "time": "Yesterday, 7:20 PM",
-        "incoming": false,
-        "video": true,
-      },
-      {
-        "name": "Pooja chauhan",
-        "time": "Tuesday, 3:15 PM",
-        "incoming": true,
-        "video": true,
-      },
-    ];
-
     return Scaffold(
-      backgroundColor: bgColor,
+      backgroundColor: Colors.black,
       appBar: AppBar(
-        backgroundColor: bgColor,
+        backgroundColor: Colors.black,
         elevation: 0,
-        iconTheme: IconThemeData(color: textColor),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.qr_code, color: Colors.white),
+            onPressed: () {},
+          ),
+          IconButton(
+            icon: Icon(Icons.search, color: Colors.white),
+            onPressed: () {},
+          ),
+          IconButton(
+            icon: Icon(Icons.more_vert, color: Colors.white),
+            onPressed: () {},
+          ),
+        ],
       ),
-      body: ListView.separated(
-        itemCount: calls.length,
-        separatorBuilder: (_, __) => Divider(
-          height: 1,
-          color: isDarkMode ? Colors.grey[800] : Colors.grey[300],
-        ),
-        itemBuilder: (context, index) {
-          final call = calls[index];
-          final String name = call['name'] ?? 'Unknown';
-          final String time = call['time'] ?? '';
-          final bool incoming = call['incoming'] ?? true;
-          final bool video = call['video'] ?? false;
-          return ListTile(
-            leading: const CircleAvatar(
-              radius: 25,
-
+      body: SingleChildScrollView(
+        padding: EdgeInsets.fromLTRB(15, 0, 15, 10),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              "Favourites",
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+              ),
             ),
-            title: Text(
-              name,
-              style: TextStyle(color: textColor, fontWeight: FontWeight.bold),
-            ),
-            subtitle: Row(
+            SizedBox(height: 10),
+            Row(
               children: [
                 Icon(
-                  incoming ? Icons.call_received : Icons.call_made,
-                  color: incoming ? Colors.red : Colors.green,
-                  size: 16,
+                  CupertinoIcons.plus_circle_fill,
+                  color: Colors.green,
+                  size: 30,
                 ),
-                const SizedBox(width: 4),
-                Text(time, style: TextStyle(color: textColor.withOpacity(0.6))),
+                SizedBox(width: 10),
+                Text(
+                  "Add favourite",
+                  style: TextStyle(color: Colors.white, fontSize: 16),
+                ),
               ],
             ),
-            trailing: Icon(
-              video ? Icons.videocam : Icons.call,
-              color: textColor,
+            SizedBox(height: 20),
+            Text(
+              "Recent",
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 18,
+                fontWeight: FontWeight.bold,
+              ),
             ),
-            onTap: () {
-            },
-          );
-        },
+            SizedBox(height: 10),
+            ListView.separated(
+              shrinkWrap: true,
+              physics: NeverScrollableScrollPhysics(),
+              itemCount: recentCalls.length,
+              separatorBuilder: (context, index) => SizedBox(height: 10),
+              itemBuilder: (context, index) {
+                var call = recentCalls[index];
+                IconData callIcon;
+                Color iconColor;
+
+                if (call["status"] == "Missed audio call") {
+                  callIcon = Icons.call_missed;
+                  iconColor = Colors.red;
+                } else if (call["status"] == "Incoming call") {
+                  callIcon = Icons.call_received;
+                  iconColor = Colors.green;
+                } else {
+                  callIcon = Icons.call_made;
+                  iconColor = Colors.green;
+                }
+
+                return Row(
+                  children: [
+                    CircleAvatar(
+                      radius: 25,
+                      backgroundImage: AssetImage(
+                        call["image"] ?? "assets/uncle.png",
+                      ),
+                    ),
+                    SizedBox(width: 10),
+                    Expanded(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            call["name"] ?? "",
+                            style: TextStyle(
+                              color:
+                                  call["status"] == "Missed audio call"
+                                      ? Colors.red
+                                      : Colors.white,
+                              fontSize: 16,
+                              fontWeight: FontWeight.w500,
+                            ),
+                          ),
+                          Row(
+                            children: [
+                              Icon(callIcon, size: 16, color: iconColor),
+                              SizedBox(width: 5),
+                              Text(
+                                call["time"] ?? "",
+                                style: TextStyle(color: Colors.white70),
+                              ),
+                            ],
+                          ),
+                        ],
+                      ),
+                    ),
+                    Icon(Icons.call, color: Colors.white60),
+                  ],
+                );
+              },
+            ),
+          ],
+        ),
       ),
       floatingActionButton: FloatingActionButton(
+        onPressed: () {},
         backgroundColor: Colors.green,
-        onPressed: () {
-        },
-        child: const Icon(Icons.add_ic_call),
+        child: Stack(
+          alignment: Alignment.center,
+          children: [
+            Icon(Icons.add_call, color: Colors.white, size: 28),
+            Positioned(
+              bottom: 4,
+              right: 4,
+              child: Icon(Icons.video_call, size: 12, color: Colors.white),
+            ),
+          ],
+        ),
       ),
     );
   }
